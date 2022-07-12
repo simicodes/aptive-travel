bbbae349bfc2f0f5bb0637b0ca341c74263f698c
simicodes <simeonoffiong@gmail.com>
2022-07-12 06:11:49 -0600

    Done with the Search component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { AiOutlineSearch } from "react-icons/ai";
-
 import beachVid from "../assets/beachVid.mp4";
 
 const Hero = () => {
@@ -9,25 +8,23 @@ const Hero = () => {
       <video
         className="w-full h-full object-cover"
         src={beachVid}
-        alt="beachVideo"
         autoPlay
         loop
         muted
       />
-      {/* Overlay............. */}
+      {/* overlay....... */}
       <div className="absolute w-full h-full top-0 left-0 bg-gray-900/30"></div>
-
       <div className="absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4">
         <h1>First Class Travel</h1>
         <h2 className="py-4">Top 1% Locations Worldwide</h2>
         <form
           className="flex justify-between items-center max-w-[700px] mx-auto w-full border p-1
-        rounded-md text-black bg-gray-100/90"
-          action=""
+          rounded-md text-black bg-gray-100/90"
         >
           <div>
             <input
-              className="bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none"
+              className="bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none
+                  "
               type="text"
               placeholder="Search Destinations"
             />
