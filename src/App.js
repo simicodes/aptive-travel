a5c4277ef7e6240f9cc7280a6b4554faa71b1968
simicodes <simeonoffiong@gmail.com>
2022-07-12 08:07:27 -0600

    Done with the Carousel component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Carousel from "./components/Carousel";
 import Destinations from "./components/Destinations";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
@@ -12,6 +13,7 @@ function App() {
       <Destinations />
       <Search />
       <Select />
+      <Carousel />
     </div>
   );
 }
